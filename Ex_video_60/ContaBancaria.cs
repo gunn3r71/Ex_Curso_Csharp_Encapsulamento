de56c2ca1cc09a6a1b52fef645694b83febd6145
Lucas <lucasgunner3@gmail.com>
2020-08-06 20:11:08 -0300

    Exercicio concluido

---
+++ Ex_video_60/ContaBancaria.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Ex_video_60
+{
+    class ContaBancaria
+    {
+        private string _titular;
+        public double Saldo { get; private set; }
+        public int Numero { get; private set; }
+
+       public ContaBancaria(int numero,string titular)
+        {
+            Numero = numero;
+            _titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double deposito_i) : this(numero,titular) //Este this é usado para
+        {                                                                                         //Poupar código
+            Saldo = deposito_i;
+        }
+
+ 
+        public string Titular
+        {
+            get
+            {
+                return _titular;
+            }
+            set
+            {
+                if (value != null && value.Length > 1) {
+                    _titular = value;
+                }
+            }
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= (quantia + 5.0);
+        }
+
+        public override string ToString()
+        {
+            return "Dados da conta atualizados:\n"
+                + "Conta " +Numero
+                +", Titular: " +Titular
+                +", Saldo: R$ "+ Saldo.ToString("F2",CultureInfo.InvariantCulture)
+                +"\n";
+        }
+    }
+}
---
+++ Ex_video_60/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Transactions;
+
+namespace Ex_video_60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria c;
+
+            Console.Write("Digite o número da conta: ");
+            int n_conta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial <s/n>? ");
+            char r_d = char.Parse(Console.ReadLine());
+            if (r_d == 's' || r_d == 'S')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double d_i = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new ContaBancaria(n_conta, titular, d_i);
+                Console.WriteLine("\nDados da conta:\nConta {0}, Titular: {1}, Saldo: R$ {2}", c.Numero, 
+                c.Titular, c.Saldo.ToString("F2", CultureInfo.InvariantCulture) + "\n");
+            }
+            else
+            {
+                c = new ContaBancaria(n_conta, titular);
+                Console.WriteLine("\n" + c + "\n");
+            }
+
+            Console.Write("Digite o valor do depósito: ");
+            double dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(dep);
+
+            Console.WriteLine(c + "\n");
+
+            Console.Write("Digite o valor do saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Saque(saque);
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
